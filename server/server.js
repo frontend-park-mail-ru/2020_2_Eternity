4af487cc4c718e333f124933f9f268dd8ed60eed
arugaf <arug113@gmail.com>
2020-10-07 04:56:03 +0300

    proxy server for api

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const proxy = require('express-http-proxy');
 const app = express()
 const port = 3000
+const apiUrl = '127.0.0.1:8008'
 
 app.use(express.static(`${__dirname}/../public`));
+
+app.use('/api', (req, res, next) => {
+    const modifiedURL = `${req.protocol}://${apiUrl}${req.url}`
+    proxy(modifiedURL)(req, res, next)
+})
 
 app.get('/', (request, response) => {
     response.sendFile(`${__dirname}/../public/index.html`)
