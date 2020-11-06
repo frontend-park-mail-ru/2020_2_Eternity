a9a138595e40cf130d993ce0b7cfc6c3d5a092b1
arugaf <arug113@gmail.com>
2020-11-06 06:30:29 +0300

    nodejs server with variable port and backend address

--- server/server.js
+++ server/server.js
@@ -1,22 +1,35 @@
-const express = require('express')
+const express = require('express');
 const proxy = require('express-http-proxy');
-const path = require('path')
-const app = express()
-const port = 80
-const apiUrl = '127.0.0.1:8008'
+const path = require('path');
+const isPortReachable = require('is-port-reachable');
+
+const app = express();
+
+const port = process.argv.length >= 3 ? process.argv[2] : 3000;
+const apiUrl = process.argv.length >= 4 ? process.argv[3] : 'localhost:8008';
+console.log('Backend address is', apiUrl);
+
+(async () => {
+    const url = apiUrl.split(':');
+    if (!await isPortReachable(apiUrl.includes(':') ? url[1] : 80, {host: url[0]})) {
+        console.log('Backend server is not working');
+        process.exit();
+    }
+})();
 
 app.use(express.static(`${__dirname}/../dist`));
 
 app.use('/api', (req, res, next) => {
-    const modifiedURL = `${req.protocol}://${apiUrl}${req.url}`
-    proxy(modifiedURL)(req, res, next)
-})
+    const modifiedURL = `${req.protocol}://${apiUrl}${req.url}`;
+    console.log(modifiedURL);
+    proxy(modifiedURL)(req, res, next);
+});
 
 app.get('/*', (request, response) => {
-    console.log(request.url)
-    response.sendFile(path.resolve(`${__dirname}/../dist/index.html`))
-})
+    console.log(request.url);
+    response.sendFile(path.resolve(`${__dirname}/../dist/index.html`));
+});
 
-app.listen(port, (err) => {
-    console.log(`server is listening on ${port}`)
-})
+app.listen(port, () => {
+    console.log(`server is listening on ${port}`);
+});
