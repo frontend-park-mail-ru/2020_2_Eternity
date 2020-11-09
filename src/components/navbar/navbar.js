027c0bd462f511a66ec979263b2203ba3aaf4c64
arugaf <arug113@gmail.com>
2020-11-09 16:48:30 +0300

    bad style

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,11 +5,15 @@ import BaseComponent from "../BaseComponent.js";
 import eventBus from "../../modules/tools/EventBus.js";
 import {Events} from "../../modules/consts/events.js";
 
+import NotificationBell from "../../components/notifications/notifications.js";
 
 class Navbar extends BaseComponent {
+    notification;
+
     constructor(context = {}) {
         super(template, context);
         this.context.isAuthenticated = false;
+        this.notification = new NotificationBell();
     }
 
     get logoutLink() {
@@ -29,8 +33,16 @@ class Navbar extends BaseComponent {
     change (data={}) {
         this.context = data;
 
+        this.context.notification = this.notification.render();
+
+
         let navbar = document.getElementById('navbar');
         navbar.innerHTML = this.render();
+
+        console.log(document.getElementById('showNotifications'))
+        document.getElementById('showNotifications').addEventListener('click', (event) => {
+            console.log(event)
+        })
 
         if (this.logoutLink) {
             this.logoutLink.addEventListener('click', this.logoutClick);
