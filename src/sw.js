f510327aac5bedf0d7fc56cf36d8a7faa3782db1
Anastasia <sanir93@gmail.com>
2020-12-27 13:57:33 +0300

    sw

--- src/sw.js
+++ src/sw.js
@@ -1,14 +1,15 @@
 let CACHE_NAME = 'offline-fallback';
+const timeout = 10000;
+
 let urlsToCache = [
-    // './',
-    './pin/',
+    './',
     './img/'
 ]
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            // return cache.addAll(urlsToCache);
+            return cache.addAll(urlsToCache);
         })
     )
     // console.log('SW Установлен');
@@ -16,14 +17,14 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(
-        caches.keys().then((cacheNames) => {
+        caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.filter((cacheName) => {
-                    return true //IF YOU WANT TO REMOVE OLD CACHE
-                }).map((cacheName) => {
-                    return caches.delete(cacheName)
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
                 })
-            )
+            );
         })
     )
     // console.log('SW Активирован');
@@ -31,19 +32,36 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            // return cachedResponse ||
-                return fetch(event.request).then((response) => {
-                // if (event.request.method === 'GET') {
-                //     return caches.open(CACHE_NAME).then((cache) => {
-                //         cache.put(event.request, response.clone());
-                //         return response;
-                //     })
-                // } else {
-                //     return response;
-                // }
-                return response;
-            })
-        })
-    )
+        fromNetwork(event.request, timeout).catch(() => fromCache(event.request))
+    );
+    event.waitUntil(update(event.request));
 });
+
+// Запрос к сети
+const fromNetwork = (request, timeout) =>
+    new Promise((fulfill, reject) => {
+        const timeoutId = setTimeout(reject, timeout);
+        fetch(request).then(response => {
+            clearTimeout(timeoutId);
+            fulfill(response);
+            if (request.method === 'GET') {
+                update(request);
+            }
+        }, reject);
+    });
+
+// Запрос к кешу
+const fromCache = (request) => {
+    caches.open(CACHE_NAME).then(cache => {
+            cache.match(request).then(matching => matching || cache.match('/offline/'))
+        }
+    );
+}
+
+// Кеширование полученного ответа если нет кеша
+const update = (request) => {
+    caches.open(CACHE_NAME).then(cache => {
+            fetch(request).then(response => cache.put(request, response))
+        }
+    );
+}
